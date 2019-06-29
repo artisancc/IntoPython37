a889451dee122b195a9f832a9db165aae175d0d5
artisancc <artisancccc@gmail.com>
2019-06-29 12:20:04 +0800

    add python-about-class folder

---
+++ python-about-class/invokeMyclass.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Invoke the MyClass Class"""
+
+from unit import *
+
+# x=Complex(1.0, -4.5)
+
+# print('x .r ',x.r,' x.i ',x.i)
+
+# p1 = People('Brisk Zhang','23','man')
+
+# print("I create an people instance,their attribute :")
+# print("Name: ",p1.name)
+# print("Age: ",p1.age)
+# print("Sex: ",p1.sex)
+
+# p2 = People('Artisan Zhang','23','man')
+
+# print("I create another people instance,their attribute :")
+# print("Name: ",p2.name)
+# print("Age: ",p2.age)
+# print("Sex: ",p2.sex)
+
+u1 = unit()
+print("I just create an object :",u1)
---
+++ python-about-class/unit.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class unit:
+
+	""" Global scope """
+
+	def __init__(self, argname, argcolor):
+		self.name = argname
+		self.color = argcolor
+
+	def __init__(selft):
+		pass
+
+	def getName():
+		return self.name
+
+	def getColor():
+		return self.color
